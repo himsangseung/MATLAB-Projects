b67dcbb3ea57e91506099837b168aecec782b2c4
himsangseung <himsangseung@hotmail.com>
2019-06-05 22:21:03 -0400

    adding Train code

---
+++ MATLAB_T/is_even.m
@@ -0,0 +1,7 @@
+function y = is_even(x)
+%is_even returns 1 if x is even
+%https://www.youtube.com/watch?v=T_ekAD7U-wU
+y = ~mod(x,2)
+    
+end
+
---
+++ MATLAB_T/sinh1.m
@@ -0,0 +1,5 @@
+function [y] = sinh1(x)
+%Example hyperpolic sign function
+y = (exp(x)-exp(-x))/2;
+end
+
